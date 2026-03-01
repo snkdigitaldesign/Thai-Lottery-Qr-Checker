72de28058d9a8bcce4b8ceef75b94635947aedcd
snkdigitaldesign <snkdigitaldesign@gmail.com>
2026-03-01 23:38:50 +0700

    Create update.js

---
+++ api/admin/update.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "API ทำงานแล้ว"
+  });
+}
