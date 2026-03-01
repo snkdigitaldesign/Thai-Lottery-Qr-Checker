106278804ff16fd09b8e3024ef1c6f1e835a66f6
snkdigitaldesign <snkdigitaldesign@gmail.com>
2026-03-02 00:05:10 +0700

    Update update.js

--- api/admin/update.js
+++ api/admin/update.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  console.log("force new build");
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
